19534cae6fce8a348c2012a3614a12c964f4f86c
joaopaulofnmvc <103937437+joaopaulofnmvc@users.noreply.github.com>
2023-02-17 16:55:12 -0300

    feat:adicionando pagina de posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>FronTips</title>
+				<title>Frontips</title>
 				<meta name="description" content="Blob,com dicas de front" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,63 @@
+import Header from "@/components/layout/Header";
+import Footer from "../../components/layout/Footer/index";
+import Wrapper from "../../components/layout/Wrapper/index";
+import Head from "next/head";
+import PostCard from "../../components/partials/PostCard/index";
+
 export default function Posts() {
-	return <div>teste</div>;
+	return (
+		<div>
+			<Head>
+				<title>Frontips Postagens</title>
+				<meta name="description" content="Frontips,postagens sobre tecnologias front-end." />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Header />
+			<main>
+				<Wrapper>
+					<div className="row">
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+
+						<div className="col-lg-4 mb-5">
+							<PostCard />
+						</div>
+					</div>
+				</Wrapper>
+				;
+			</main>
+			<Footer />
+		</div>
+	);
 }
