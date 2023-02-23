f9941148939f953642f96c07995d78a581ec66f9
joaopaulofnmvc <103937437+joaopaulofnmvc@users.noreply.github.com>
2023-02-23 20:59:11 -0300

    fix:ajustando responsividade initial scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
